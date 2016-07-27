e8bacf5e38963ffb99919c471cb6e3d906a34f6c
pawankumar1008 <pawankumar1008@gmail.com>
2016-07-27 16:35:24 +0530

    Created by Coding Ground Client

---
+++ New Project-20160727/MyRunnableThreadClass.java
@@ -0,0 +1,10 @@
+class MyRunnableThreadClass  implements Runnable
+{
+    public void run(){
+        System.out.println("inside MyRunnableThreadClass ");
+       // System.out.println(Thread.getName());
+       
+ 
+    }
+
+}
